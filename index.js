b9ca26eb35f6e6048f8d3678a77fc01fc156d145
Charberry <91689718+Charberry08@users.noreply.github.com>
2023-09-15 12:13:29 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var menuList = document.getElementById("menu-list");
+
+menuList.style.maxHeight = "0px";
+
+function toggleMenu()
+{
+    if(menuList.style.maxHeight == "0px")
+    {
+        menuList.style.maxHeight = "130px";
+    }
+    else
+    {
+        menuList.style.maxHeight = "0px";
+    }
+}
+
+function tochoIncBarIconAction()
+{
+    window.location.reload();
+}
